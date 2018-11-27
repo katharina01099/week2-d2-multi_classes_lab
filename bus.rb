0422ad68be64e8bf162e35583859607f4ca13dac
Katharina01099 <yes>
2018-11-27 16:15:19 +0000

    add person1 to 3

--- bus.rb
+++ bus.rb
@@ -16,5 +16,12 @@ class Bus
     return @passengers.length
   end
 
+  def pick_up(person)
+    @passenger << person
+  end
+
+
+# Add a method to the Bus class which takes in a Person and adds it to the array of passengers.
+# The method could look something like bus.pick_up(passenger1)
 
 end
--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -2,6 +2,7 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../bus")
+require_relative("../person")
 
 class BusTest < MiniTest::Test
 
@@ -9,6 +10,9 @@ class BusTest < MiniTest::Test
     route_number = 22
     destination = "Ocean Terminal"
     @bus = Bus.new(route_number, destination)
+    @passenger1 = Person.new("Bob", 42)
+    @passenger2 = Person.new("Audrey", 81)
+    @passenger3 = Person.new("Johnny", 18)
   end
 
   def test_driving_sound
@@ -19,5 +23,9 @@ class BusTest < MiniTest::Test
   def test_number_of_passengers
     assert_equal(0, @bus.number_of_passengers)
   end
+
+
+
+
 
 end
